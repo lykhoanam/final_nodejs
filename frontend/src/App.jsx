889c0227deac064b9ab275f4da1eea188e1d42ec
Ng T.Nguyen <nguyenthanhnguyen17102003@gmail.com>
2024-12-04 19:57:49 +0700

    fix bug header footer route in App and main files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, createContext, useEffect} from "react";
 import {gapi} from 'gapi-script';
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
 
 import AnnouncementBar from "./components/AnnouncementBar";
 import Header from "./components/Header";
@@ -15,10 +15,6 @@ import Profile from "./components/profile/ProfilePage.jsx"
 import Recovery from "./components/login/VerifyOTP.jsx"
 import CheckOut from "./components/cart/CheckOut.jsx"
 import Admin from "./components/admin/Admin.jsx";
-
-import TableUsers from "./components/admin/Table/TableUsers.jsx";
-import ProductTable from "./components/admin/Table/TableProducts.jsx";
-
 export const Context = createContext();
 
 const clientId = "1074315812564-92s9klc0eos45ujtefj613bkualvulq0.apps.googleusercontent.com";
@@ -29,7 +25,9 @@ function App() {
     const location = useLocation();
 
     const adminRoute = "/admin"
-    const shouldHideHeaderFooter = adminRoute.includes(location.pathname);
+    const shouldHideHeaderFooter = location.pathname.includes(adminRoute);
+
+    console.log(shouldHideHeaderFooter)
     useEffect(() => {
       function start(){
         gapi.client.init({
@@ -41,80 +39,10 @@ function App() {
       gapi.load('client:auth2', start)
     });
 
-    const tableData = [
-      { fullName: "John Doe", phone: "123-456-7890", email: "john@example.com", address: "123 Main St" },
-      { fullName: "Jane Smith", phone: "987-654-3210", email: "jane@example.com", address: "456 Oak Ave" },
-      { fullName: "Sam Johnson", phone: "555-555-5555", email: "sam@example.com", address: "789 Pine Rd" },
-      { fullName: "John Doe", phone: "123-456-7890", email: "john@example.com", address: "123 Main St" },
-      { fullName: "Jane Smith", phone: "987-654-3210", email: "jane@example.com", address: "456 Oak Ave" },
-      { fullName: "Sam Johnson", phone: "555-555-5555", email: "sam@example.com", address: "789 Pine Rd" },
-  ];
-  
-    const productData = [
-      {
-        id: 1,
-        name: "Perfume Essence No.1",
-        quantity: 50,
-        star: 4.5,
-        description: "A luxurious fragrance with a blend of floral and woody notes.",
-        discount: 0,
-        image: "https://i.ibb.co/126PHYL/0369446fe4f54cebd4bbb403a111ce9b.jpg",
-        category: "For Her",
-        variants: [
-          { size: "50ml", price: 3520000 },
-          { size: "100ml", price: 7520000 },
-        ],
-      },
-      {
-        id: 2,
-        name: "Mystic Bloom",
-        quantity: 30,
-        star: 4.2,
-        description: "A refreshing scent inspired by blooming gardens.",
-        discount: 0,
-        image: "https://i.ibb.co/Ntn63s6/0c438b169aac2ca69cbe5df6f69261a2.jpg",
-        category: "For Her",
-        variants: [
-          { size: "50ml", price: 3520000 },
-          { size: "100ml", price: 7520000 },
-        ],
-      },
-      {
-        id: 3,
-        name: "Elegant Whisper",
-        quantity: 40,
-        star: 4.8,
-        description: "Sophisticated and subtle, perfect for evening wear.",
-        discount: 0,
-        image: "https://i.ibb.co/vwB2rgr/713ff5fccfb22ef0eed5afeb65158041.jpg",
-        category: "For Her",
-        variants: [
-          { size: "50ml", price: 3520000 },
-          { size: "100ml", price: 7520000 },
-        ],
-      },
-      {
-        id: 4,
-        name: "Ocean Breeze",
-        quantity: 25,
-        star: 4.0,
-        description: "A light and breezy fragrance reminiscent of the sea.",
-        discount: 0,
-        image: "https://i.ibb.co/VH99QKG/e679333e48b1444a87d7fd7c165c0139.jpg",
-        category: "For Her",
-        variants: [
-          { size: "50ml", price: 3520000 },
-          { size: "100ml", price: 7520000 },
-        ],
-      },
-    ]
-
     return (
         <Context.Provider value={[cartCounter, setCartCounter]}>
-            {/* <AnnouncementBar title="Free Shipping in Europe" /> */}
-            {!shouldHideHeaderFooter && <Header navigationItems={navigationItems} />}
-            
-          
+              {/* <AnnouncementBar title="Free Shipping in Europe" /> */}
+          <Header navigationItems={navigationItems} hidden={shouldHideHeaderFooter}/> 
                 <Routes>
                     <Route path="/recovery" element={<Recovery/>}></Route>
                     <Route path="/profile" element={<Profile />}></Route>
@@ -199,13 +127,9 @@ function App() {
                  
                     {/* Định tuyến tới trang admin */}
                     <Route path="/admin" element={<Admin/>}/>
-                      
-          
-
-   
                 </Routes>
-            {!shouldHideHeaderFooter && <Footer />}
-        </Context.Provider>
+              <Footer />            
+            </Context.Provider>
 
         
     );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
-import { BrowserRouter, Router } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
-            
+                <App />      
         </BrowserRouter>
-
-        
     </React.StrictMode>
 );
